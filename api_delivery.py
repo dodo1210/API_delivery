ef4fc1f3187230d61742b31cec9346bc558b02d1
Douglas Siqueira Gonçalves <ds417042@gmail.com>
2021-02-01 17:01:08 -0300

    Criado validações para saída

--- api_delivery.py
+++ api_delivery.py
@@ -1,26 +1,40 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__) 
 
-deliveries = []
+
+def weight_most_zero(weight):
+	if weight<=0:
+		return []
+	return weight
+
+def min_max(height,width):
+	print(height,width)
+	if height < 5 or height> 200 or width<6 or width>140:
+		return 0
+	if height>=5 and height<10:
+		return 1
+	if (width>=6 and width<=13) or (width>=125 and width<=140) or (height>=140 and height<=200):
+		return 2
+
 
 @app.route('/payload', methods=['POST'])
 def payload():
+	size = request.json['dimensao']
+	if weight_most_zero(request.json['peso']) == []:
+		return jsonify([])
+	if min_max((jsonify(size).json['altura']),(jsonify(size).json['largura'])) == 0:
+		return jsonify([])
+	if min_max((jsonify(size).json['altura']),(jsonify(size).json['largura'])) == 2:
+		return jsonify({"nome":"Entrega Ninja","valor_frete": (request.json['peso']*0.3)/10,"prazo_dias": 6})
+	if min_max((jsonify(size).json['altura']),(jsonify(size).json['largura'])) == 1:
+		return jsonify({"nome":"Entrega Especial","valor_frete": (request.json['peso']*0.2)/10,"prazo_dias": 4})	
 
-	weight = request.json['peso']
+	delivery = {"nome":"Entrega Ninja","valor_frete": (request.json['peso']*0.3)/10,"prazo_dias": 6},{"nome":"Entrega Especial","valor_frete": (request.json['peso']*0.2)/10,"prazo_dias": 4}
+	return jsonify(delivery)
 
-	delivery = {
-        "nome":"Entrega Ninja",
-    	"valor_frete": (weight*0.3)/10,
-    	"prazo_dias": 6
-	},{
-        "nome":"Entrega Especial",
-    	"valor_frete": (weight*0.2)/10,
-    	"prazo_dias": 4
-	}
-	return jsonify({'dimensão':delivery})
 
 if __name__ == '__main__':
-	app.run(debug=True, port=8080) 
+	app.run(debug=True, port=8080) #run app on port 8080 in debug model
 
 #size = request.json['dimensao']
 #height = (jsonify(size).json['altura'])
