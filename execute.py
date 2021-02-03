db7f19a8213d3af13dc9de10fd064cb378d6ccf1
Douglas Siqueira Gonçalves <ds417042@gmail.com>
2021-02-03 07:40:33 -0300

    Modularização em pastas

--- execute.py
+++ execute.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from api_delivery import my_application_bp
+from app.api_delivery import my_application_bp
 
 app = Flask(__name__) 
 app.register_blueprint(my_application_bp)
 
+
 if __name__ == '__main__':
-	app.run(debug=True, port=8080) #run app on port 8080 in debug model
+	app.run(debug=True)
