a169cb2b73a62ffcd01b6ba9f2e20dae6aa97750
Douglas Siqueira Gonçalves <ds417042@gmail.com>
2021-02-03 16:15:38 -0300

    Editando os limites

--- app/api_delivery.py
+++ app/api_delivery.py
@@ -7,15 +7,13 @@ def payload():
 	if request.json['peso'] <= 0:
 		return jsonify([])
 		height < 5 or height> 200 or width<6 or width>140
-	if jsonify(size).json['altura']<5 or jsonify(size).json['altura']>140 or jsonify(size).json['largura'] < 6 or jsonify(size).json['largura'] > 200:
+	if jsonify(size).json['altura']<5 or jsonify(size).json['altura']>200 or jsonify(size).json['largura'] < 6 or jsonify(size).json['largura'] > 140:
 		return jsonify([])
-	if  (jsonify(size).json['largura']>=6 and jsonify(size).json['largura']<13) or (jsonify(size).json['largura']>125 and jsonify(size).json['largura']<140) or (jsonify(size).json['altura']>=140 and jsonify(size).json['altura']<=200):
+	if  (jsonify(size).json['largura']>=6 and jsonify(size).json['largura']<=13) or (jsonify(size).json['largura']>=125 and jsonify(size).json['largura']<=140) or (jsonify(size).json['altura']>140 and jsonify(size).json['altura']<=200):
 		return jsonify([{"nome":"Entrega Ninja","valor_frete": (request.json['peso']*0.3)/10,"prazo_dias": 6}])
 	if (jsonify(size).json['altura']>=5 and jsonify(size).json['altura']<10):
 		return jsonify([{"nome":"Entrega Kabum","valor_frete": (request.json['peso']*0.2)/10,"prazo_dias": 4}])	
 
 	delivery = {"nome":"Entrega Ninja","valor_frete": (request.json['peso']*0.3)/10,"prazo_dias": 6},{"nome":"Entrega Kabum","valor_frete": (request.json['peso']*0.2)/10,"prazo_dias": 4}
 	return jsonify(delivery)
-
-
 
