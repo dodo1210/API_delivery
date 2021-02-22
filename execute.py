063c07c3a0cd4a1a8067173d4d24e69e944947be
dodo1210 <ds417042@gmail.com>
2021-02-22 10:56:44 -0300

    teste

--- execute.py
+++ execute.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from app.api_delivery import my_application_bp
+import os
 
 app = Flask(__name__) 
 app.register_blueprint(my_application_bp)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True) 
